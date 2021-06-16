1fe2f42ea20e9215d814e5d8403140f93737f7c4
huangchengwen <huangchengwen@hetao101.com>
2021-06-16 20:23:53 +0800

    feature: 添加示例

---
+++ src/example/components/Main/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { useEffect } from 'react';
+
+
+export default function IndexPage() {
+  useEffect(() => {
+   
+  }, []);
+
+  return (
+    <div>
+        <h1>hello world</h1>
+    </div>
+  );
+}
